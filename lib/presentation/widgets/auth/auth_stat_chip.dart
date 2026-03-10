3b7adc37ca73aa65fe6341f683f0cea3d9397249
rcshpark <rcshpark@gmail.com>
2026-03-10 21:22:35 +0900

    feat: add AuthStatChip widget for reusable authentication status display

---
+++ lib/presentation/widgets/auth/auth_stat_chip.dart
@@ -0,0 +1,22 @@
+import 'package:ai_news_analyzer/core/const/app_color_const.dart';
+import 'package:ai_news_analyzer/core/const/app_text_style_const.dart';
+import 'package:flutter/material.dart';
+
+class AuthStatChip extends StatelessWidget {
+  const AuthStatChip({required this.label, super.key});
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      decoration: BoxDecoration(
+        color: AppColors.iconBackground,
+        borderRadius: BorderRadius.circular(14),
+        border: Border.all(color: AppColors.panelBorder),
+      ),
+      child: Text(label, style: AppTextStyles.heroStat),
+    );
+  }
+}
